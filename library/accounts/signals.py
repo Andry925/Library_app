9057ef322416d051e02add0c671b434a81c7f768
Andrew925 <andry925@ukr.net>
2023-12-07 22:26:05 +0200

    Fixing issue with incorrect userprofile updating

--- library/accounts/signals.py
+++ library/accounts/signals.py
@@ -2,8 +2,9 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import User, UserProfile
 
-@receiver(post_save, sender = User)
-def post_save_funct(sender,instance,created,**kwargs):
+
+@receiver(post_save, sender=User)
+def post_save_funct(instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     else:
@@ -11,9 +12,9 @@ def post_save_funct(sender,instance,created,**kwargs):
             updated_user_profile = UserProfile.objects.get(user=instance)
             updated_user_profile.save()
         except UserProfile.DoesNotExist:
-            UserProfile.objects.create()
+            UserProfile.objects.create(user=instance)
 
-@receiver(pre_save, sender = User)
-def pre_save_funct(sender, instance, **kwargs):
+
+@receiver(pre_save, sender=User)
+def pre_save_funct(instance,**kwargs):
     print(instance.username, "The user is created")
-       
