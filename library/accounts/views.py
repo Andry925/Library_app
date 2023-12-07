239b08b8311787e6269daaff9de58dff4c5d6826
Andrew925 <andry925@ukr.net>
2023-12-07 15:36:51 +0200

    Added basic view for registration

--- library/accounts/views.py
+++ library/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 # Create your views here.
+def registration(request):
+    user_form = UserForm()
+    context = {
+        "user_form":user_form
+    }
+    return render(request, "accounts/register.html",context)
