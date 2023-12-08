86e4e81103ee2991524c8392b83cd34bc6d4826b
Andrew925 <andry925@ukr.net>
2023-12-08 12:35:54 +0200

    Adding basic registration from registration page

--- library/accounts/views.py
+++ library/accounts/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import UserForm
 
 # Create your views here.
+
+
 def registration(request):
-    user_form = UserForm()
-    context = {
-        "user_form":user_form
-    }
-    return render(request, "accounts/register.html",context)
+    if request.method != "POST":
+        user_form = UserForm()
+        context = {
+            "user_form": user_form
+        }
+        return render(request, "accounts/register.html", context)
+    user_form_with_data = UserForm(request.POST)
+
+    if not user_form_with_data.is_valid():
+        context = {
+            "user_form": user_form_with_data
+        }
+        return render(request, "accounts/register.html", context)
+    user_form_with_data.save()
+    return redirect("register")
