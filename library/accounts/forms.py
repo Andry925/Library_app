59b5b270c362d76b607dcd847fb2a05d3f469621
Andrew925 <andry925@ukr.net>
2023-12-08 15:49:06 +0200

    Added logic to manage non fields error during registration

--- library/accounts/forms.py
+++ library/accounts/forms.py
@@ -1,7 +1,28 @@
+from typing import Any
 from django import forms
 from .models import User
 
+
 class UserForm(forms.ModelForm):
-    class Mete:
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
         model = User
-        fields = ["email","first_name","last_name","username","role","password"]
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+            "username",
+            "role",
+            "password"]
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Passwords do not match"
+            )
