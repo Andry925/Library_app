54f92d00a46a133959b187a6ccb461d921c6df8a
Andrew925 <andry925@ukr.net>
2023-12-10 22:55:58 +0200

    Adding basic login

--- library/accounts/views.py
+++ library/accounts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect, render
 from .forms import UserForm
-
-# Create your views here.
+from django.contrib.auth import authenticate, login
 
 
 def registration(request):
@@ -15,15 +14,33 @@ def registration(request):
 
     if not user_form_with_data.is_valid():
         context = {
-            "user_form": user_form_with_data
-        }
+            "user_form": user_form_with_data}
         return render(request, "accounts/register.html", context)
     object_with_hashed_password = hash_password(user_form=user_form_with_data)
     object_with_hashed_password.save()
     return redirect("register")
 
+
 def hash_password(user_form):
     password = user_form.cleaned_data["password"]
-    user = user_form.save(commit = False)
+    user = user_form.save(commit=False)
     user.set_password(password)
     return user
+
+
+def login_user(request):
+    if request.method != "POST":
+        return render(request, "accounts/login.html")
+
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    user = authenticate(email=email, password=password)
+    if user:
+        login(request, user)
+        return redirect("userprofile")
+    raise ValueError()
+
+
+def dashboard(request):
+    return render(request, "accounts/userprofile.html")
