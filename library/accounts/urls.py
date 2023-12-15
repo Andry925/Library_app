e92440fea0cb8aed0d640a3714548e23133cbc18
Andrew925 <andry925@ukr.net>
2023-12-15 20:37:21 +0200

    Trying to implement class based registation process to refactor code

--- library/accounts/urls.py
+++ library/accounts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/",views.registration,name="register"),
-    path("login/",views.login_user, name ="login"),
-    path("logout/",views.logout_user,name="logout"),
-    path("librarian_profile/",views.redirect_librarian, name="librarian_profile"),
-    path("user_profile/", views.redirect_user, name = "user_profile")
+    path("register/",views.RegistrationView.as_view(),name="register"),
+    #path("login/",views.LoginView.as_view(), name ="login"),
+    #path("logout/",views.logout_user,name="logout"),
+    #path("librarian_profile/",views.redirect_librarian, name="librarian_profile"),
+    #path("user_profile/", views.redirect_user, name = "user_profile")
     
 ]
--- library/accounts/views.py
+++ library/accounts/views.py
@@ -1,65 +1,28 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from . utils import manage_user
+from django.shortcuts import render, redirect
+from django.views import View
 from .forms import UserForm
 
 
-def registration(request):
-    if request.method != "POST":
-        user_form = UserForm()
-        context = {
-            "user_form": user_form
-        }
-        return render(request, "accounts/register.html", context)
-    user_form_with_data = UserForm(request.POST)
+class RegistrationView(View):
+    template_name = "accounts/register.html"
+    user_form = UserForm
 
-    if not user_form_with_data.is_valid():
-        context = {
-            "user_form": user_form_with_data}
-        return render(request, "accounts/register.html", context)
-    object_with_hashed_password = hash_password(user_form=user_form_with_data)
-    object_with_hashed_password.save()
-    return redirect("register")
+    def get(self, request):
+        registration_form = self.user_form()
+        context = {"registration_form": registration_form}
+        return render(request, self.template_name, context)
 
+    def post(self, request):
+        registration_form = self.user_form(request.POST)
+        if registration_form.is_valid():
+            user_with_hashed_password = self.hash_password(registration_form)
+            user_with_hashed_password.save()
+            return redirect("register")
+        context = {"registration_form": registration_form}
+        return render(request, self.template_name, context)
 
-def hash_password(user_form):
-    password = user_form.cleaned_data["password"]
-    user = user_form.save(commit=False)
-    user.set_password(password)
-    return user
-
-
-def login_user(request):
-    if request.method != "POST":
-        return render(request, "accounts/login.html")
-
-    email = request.POST.get("email")
-    password = request.POST.get("password")
-    user = authenticate(email=email, password=password)
-    if user:
-        login(request, user)
-        return determine_appropraite_profile(request)
-
-
-@login_required(login_url="login")
-def logout_user(request):
-    logout(request)
-    return redirect("login")
-
-
-@login_required(login_url="login")
-def determine_appropraite_profile(request):
-    user = request.user
-    user_url = manage_user(user)
-    return redirect(user_url)
-
-
-@login_required(login_url="login")
-def redirect_user(request):
-    return render(request, "accounts/userprofile.html")
-
-
-@login_required(login_url="login")
-def redirect_librarian(request):
-    return render(request, "accounts/librarianprofile.html")
+    def hash_password(self, valid_user_form):
+        password = valid_user_form.cleaned_data.get("password")
+        user = valid_user_form.save(commit=False)
+        user.set_password(password)
+        return user
