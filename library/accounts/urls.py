8628cf1eb9bee3e6e21ae55e89d98cf9cf0ff2d5
Andrew925 <andry925@ukr.net>
2023-12-15 21:00:36 +0200

    Adding login functionality

--- library/accounts/urls.py
+++ library/accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("register/",views.RegistrationView.as_view(),name="register"),
-    #path("login/",views.LoginView.as_view(), name ="login"),
+    path("login/",views.LoginView.as_view(), name ="login"),
     #path("logout/",views.logout_user,name="logout"),
     #path("librarian_profile/",views.redirect_librarian, name="librarian_profile"),
     #path("user_profile/", views.redirect_user, name = "user_profile")
--- library/accounts/views.py
+++ library/accounts/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.views import View
+from library.accounts.utils import manage_user
 from .forms import UserForm
 
 
@@ -26,3 +28,18 @@ class RegistrationView(View):
         user = valid_user_form.save(commit=False)
         user.set_password(password)
         return user
+
+
+class LoginView(View):
+    template_name = "accounts/login.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        if user:
+            login(request, user)
+        return render(request, self.template_name)
