b30314114753eb1f4a6b8b0537a8ef7241cc71ba
Andrew925 <andry925@ukr.net>
2023-12-16 21:41:19 +0200

    Adding function to display authors

--- library/author/views.py
+++ library/author/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Author
 
 # Create your views here.
+
+def index(request):
+    authors = Author.get_all_authors()
+    context = {"authors":authors}
+    return render(request,"author/authors_page.html",context)
+
+
