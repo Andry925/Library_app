cfe7eeafb7907fc737b429e382e1fc258937385c
Andrew925 <andry925@ukr.net>
2023-12-16 12:48:50 +0200

    Adding basic author model

--- library/author/models.py
+++ library/author/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    author_name = models.CharField(max_length=20)
+    author_surname = models.CharField(max_length=20, unique=True)
+    patronymic = models.CharField(blank=True, max_length=20)
+
+    def __str__(self):
+        return f"{self.author_name} {self.author_surname}"
