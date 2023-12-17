e77e7e4b722b456621045ab82aedbc98350ac9e5
Andrew925 <andry925@ukr.net>
2023-12-17 13:03:31 +0200

    function and urls for creating author

--- library/author/urls.py
+++ library/author/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("authors/",views.index,name="all_authors"),
+    path("add_author/",views.create_new_author, name="author_creation_form"),
 ]
--- library/author/views.py
+++ library/author/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Author
 
-# Create your views here.
 
 def index(request):
     authors = Author.get_all_authors()
-    context = {"authors":authors}
-    return render(request,"author/authors_page.html",context)
+    context = {"authors": authors}
+    return render(request, "author/authors_page.html", context)
 
 
+def create_new_author(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        patronymic = request.POST.get('patronymic')
+        Author.create_author(name=name, surname=surname, patronymic=patronymic)
+        return redirect('author_creation_form')
+    return render(request, "author/new_author.html")
