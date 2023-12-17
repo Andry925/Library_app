8f7c530fa090f0fbcb8e98d8b554b4395cc790b9
Andrew925 <andry925@ukr.net>
2023-12-17 15:25:27 +0200

    Dividing logic between two classes

--- library/author/views.py
+++ library/author/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Author
 
 
-class AllUsers(View):
+class AllUsersView(View):
     template_path = "author/authors_page.html"
-    redirect_url = "author_creation_form"
 
     def get(self, request):
         all_users = Author.get_all_authors()
         context = {"all_users", all_users}
         return render(request, self.template_path, context)
+
+
+class CreateUserView(View):
+    template_path = "author/new_author.html"
+    redirect_url = "author_creation_form"
+
+    def get(self, request):
+        return render(request, self.template_path)
