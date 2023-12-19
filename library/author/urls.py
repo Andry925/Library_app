16b37a085dd7f57a1bd5d33d9101c093001ce6ab
Andrew925 <andry925@ukr.net>
2023-12-19 20:36:30 +0200

    Adding urls routes

--- library/author/urls.py
+++ library/author/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("authors/",views.index,name="all_authors"),
-    path("add_author/",views.create_new_author, name="author_creation_form"),
+    path("authors/", views.AllUsersView.as_view(), name="all_authors"),
+    path("add_author/", views.CreateUserView.as_view(), name="author_creation_form"),
 ]
