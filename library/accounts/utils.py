aa62b32bd7d3e5e7cb1fab8711728adaabe3329a
Andrew925 <andry925@ukr.net>
2023-12-19 20:38:58 +0200

    Updating utils

--- library/accounts/utils.py
+++ library/accounts/utils.py
@@ -3,5 +3,5 @@ def manage_user(user):
     if user.role == "Librarian":
         user_profile = "librarian_profile"
         return user_profile
-    user_profile = "librarian_profile"
+    user_profile = "user_profile"
     return user_profile
