2032b0d9a1723fcae3dc70655df4dd777903df21
Andrew925 <andry925@ukr.net>
2023-12-22 20:43:10 +0200

    Function to update author's information

--- library/author/views.py
+++ library/author/views.py
@@ -7,18 +7,19 @@ from .models import Author
 
 class AllUsersView(View):
     template_path = "author/authors_page.html"
+
     def get(self, request):
         all_authors = Author.get_all_authors()
         context = {"all_authors": all_authors}
         return render(request, self.template_path, context)
-    
+
+
 class CreateUserView(View):
     template_path = "author/new_author.html"
     redirect_url = "author_creation_form"
 
-    def get(self,request):
+    def get(self, request):
         return render(request, self.template_path)
-
 
     def post(self, request):
         try:
@@ -32,15 +33,24 @@ class CreateUserView(View):
             messages.error(request, error_message)
             return render(request, "author/new_author.html")
 
+
 class AuthorUpdateView(View):
     template_path = "author/edit_author.html"
+    redirect_url = "all_authors"
 
-    def get(self,request,pk):
+    def get(self, request, pk):
         author = Author.get_author_by_id(pk=pk)
-        context = {"author":author}
-        return render(request,self.template_path,context)
+        context = {"author": author}
+        return render(request, self.template_path, context)
+
+    def post(self, request, pk):
+        fields = ("name", "surname", "patronymic")
+        author = Author.get_author_by_id(pk=pk)
+        for field in fields:
+            setattr(author, field, request.POST.get(field))
+        author.save()
+        return redirect(self.redirect_url)
 
 
 class AuthorDeleteView(View):
     pass
-            
