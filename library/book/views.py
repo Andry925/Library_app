d10beb77b8b77f5888310c5e55872da005633190
Andrew925 <andry925@ukr.net>
2023-12-31 12:37:58 +0200

    Adding class to display all_books page

--- library/book/views.py
+++ library/book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Book
 
-# Create your views here.
+
+class AllBooksView(View):
+    template_path = 'book/all_books.html'
+
+    def get(self, request):
+        all_books = Book.get_all_books()
+        context = {'all_books': all_books}
+        return render(request, self.template_path, context)
