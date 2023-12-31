b1d4c1d9fbbdc41bcc1985a73fb37f0c6cab7dc1
Andrew925 <andry925@ukr.net>
2023-12-31 19:22:42 +0200

    Adding functionality to view book edit from with urls

--- library/book/urls.py
+++ library/book/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("all_books/", views.AllBooksView.as_view(), name="all_books")
+    path("all_books/", views.AllBooksView.as_view(), name="all_books"),
+    path("add_book",views.AddBookView.as_view(), name="add_book")
 
 ]
--- library/book/views.py
+++ library/book/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Book
+from .forms import BookCreationForm
 
 
 class AllBooksView(View):
-    template_path = 'book/all_books.html'
+    template_path = "book/all_books.html"
 
     def get(self, request):
         all_books = Book.get_all_books()
-        context = {'all_books': all_books}
+        context = {"all_books": all_books}
+        return render(request, self.template_path, context)
+
+
+class AddBookView(View):
+    template_path = "book/add_new_book.html"
+    book_form = BookCreationForm
+
+    def get(self, request):
+        book_creation_form = self.book_form()
+        context = {"book_form": book_creation_form}
         return render(request, self.template_path, context)
