bf6f664086fd4f717162e12910f68e29ffbbb4d8
Andrew925 <andry925@ukr.net>
2024-01-02 20:19:08 +0200

    Editing count field for positive numbers

--- library/book/models.py
+++ library/book/models.py
@@ -5,7 +5,7 @@ from author.models import Author
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20, unique=True)
-    count = models.IntegerField(default=0)
+    count = models.PositiveIntegerField(default=0)
     genre = models.CharField(max_length=10, blank=True)
     author = models.ForeignKey(
         Author,
@@ -26,4 +26,14 @@ class Book(models.Model):
 
     @staticmethod
     def get_all_books():
-        return Book.objects.all()
+        return Book.objects.all().order_by("id")
+
+    @staticmethod
+    def delete_book(pk):
+        try:
+            book = Book.objects.get(pk=pk)
+            book.delete()
+            return True
+
+        except Book.DoesNotExist:
+            return False
