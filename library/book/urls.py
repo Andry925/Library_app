e94b9d6293fc78af8e6322ed5bb8f2bfab791b62
Andrew925 <andry925@ukr.net>
2024-01-04 12:30:45 +0200

    Adding url configuration

--- library/book/urls.py
+++ library/book/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("all_books/", views.AllBooksView.as_view(), name="all_books"),
-    path("add_book",views.AddBookView.as_view(), name="add_book")
+    path("add_book", views.AddBookView.as_view(), name="add_book"),
+    path("filter_book/", views.FilteredBooksView.as_view(), name="filter_book")
 
 ]
