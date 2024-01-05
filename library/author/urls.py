ba716fc1a79a463f090c67a743b4f877e5c9e579
Andrew925 <andry925@ukr.net>
2024-01-05 13:04:50 +0200

    Editing class names in urls

--- library/author/urls.py
+++ library/author/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("authors/", views.AllUsersView.as_view(), name="all_authors"),
-    path("add_author/", views.CreateUserView.as_view(), name="author_creation_form"),
+    path("authors/", views.AllAuthorsView.as_view(), name="all_authors"),
+    path("add_author/", views.CreateAuthorsView.as_view(), name="author_creation_form"),
     path("edit_author/<int:pk>/",views.AuthorUpdateView.as_view(), name="edit_author"),
     path("delete_author/<int:pk>/",views.AuthorDeleteView.as_view(),name="delete_author")
 ]
