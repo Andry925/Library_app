4a8607d51020eb342f83b285c8f18bb352093add
Andrew925 <andry925@ukr.net>
2024-01-05 20:46:56 +0200

    Adding rest of the tests for author

--- library/author/tests.py
+++ library/author/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.db import IntegrityError, transaction
 from .models import Author
 
 
@@ -15,17 +16,22 @@ class AuthorViewsTestCase(TestCase):
         self.edit_url = reverse("edit_author", args=[self.author.pk])
         self.delete_url = reverse("delete_author", args=[self.author.pk])
 
-    def test_all_authors_view(self):
+    def test_all_authors_get(self):
         response = self.client.get(reverse("all_authors"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "author/authors_page.html")
 
-    def test_create_authors_view_get(self):
+    def test_create_author_get(self):
         response = self.client.get(self.create_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "author/new_author.html")
 
-    def test_create_authors_view_post(self):
+    def test_edit_author_get(self):
+        response = self.client.get(self.edit_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "author/edit_author.html")
+
+    def test_create_author_post(self):
         response = self.client.post(self.create_url, data=self.author_data)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "author/new_author.html")
@@ -37,5 +43,15 @@ class AuthorViewsTestCase(TestCase):
             "patronymic": "Grigorovich",
         }
         response = self.client.post(self.edit_url, data=updated_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("all_authors"))
+
+    def test_duplicate_surname_post(self):
+        with transaction.atomic():
+            response = self.client.post(self.create_url, data=self.author_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_author_delete(self):
+        response = self.client.get(self.delete_url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("all_authors"))
