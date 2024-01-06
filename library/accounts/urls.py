45daaca8ef74b65d7d5ffeb673d0fd2cf722a7b8
Andrew925 <andry925@ukr.net>
2024-01-06 21:21:03 +0200

    Editing url to automatically appear on registration page

--- library/accounts/urls.py
+++ library/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/",views.RegistrationView.as_view(),name="register"),
+    path("",views.RegistrationView.as_view(),name="register"),
     path("login/",views.LoginView.as_view(), name ="login"),
     path("user_profile/", views.UserProfileView.as_view(), name = "user_profile"),
     path("librarian_profile/",views.LibrarianProfileView.as_view(), name="librarian_profile"),
