b61b99396bc264e533518f77eaad8b35e48ef86b
Andrew Tymoshenko <andry925@ukr.net>
2024-01-14 13:26:37 +0200

    Adding function to display page

--- library/order/views.py
+++ library/order/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Order
 
-# Create your views here.
+
+class AllOrdersView(View):
+    template_path = "order/available_orders.html"
+
+    def get(self, request):
+        all_orders = Order.get_all_orders()
+        context = {
+            "all_orders": all_orders
+        }
+        return render(request, self.template_path, context)
