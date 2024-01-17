b3298c002c1033730684baf1f7c5997d49f8bed9
Andrew Tymoshenko <andry925@ukr.net>
2024-01-17 13:00:27 +0200

    Adding class to manage users permissons

--- library/accounts/utils.py
+++ library/accounts/utils.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
+
 
 def manage_user(user):
     if user.role == "Librarian":
@@ -5,3 +8,10 @@ def manage_user(user):
         return user_profile
     user_profile = "user_profile"
     return user_profile
+
+
+class TestMixIn(UserPassesTestMixin):
+    def test_func(self):
+        if self.request.user.role == "Librarian":
+            return True
+        raise PermissionDenied("You are not allowed to view this page")
