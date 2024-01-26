1d30bd0340ddc635f1f336ed015fb980d21f9e51
Andrew Tymoshenko <andry925@ukr.net>
2024-01-26 21:57:48 +0200

    Adding urls and html page to create an order

--- library/order/urls.py
+++ library/order/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("available_orders/", views.AllOrdersView.as_view(), name = "available_orders"),
+    path("create_order/", views.CreateOrderView.as_view(), name = "create_order")
 ]
