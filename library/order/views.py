f2998202166b7345a3fa9b9648b30baa0647ab7f
Andrew Tymoshenko <andry925@ukr.net>
2024-01-27 12:21:07 +0200

    Adding function to save order along with current user

--- library/order/views.py
+++ library/order/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import Order
+from .forms import OrderCreateForm
 
 
 @method_decorator(login_required(login_url="login"), name="dispatch")
@@ -13,5 +14,31 @@ class AllOrdersView(View):
         all_orders = Order.get_all_orders()
         context = {
             "all_orders": all_orders
+        }
+        return render(request, self.template_path, context)
+
+
+class CreateOrderView(View):
+    template_path = "order/order_creation_form.html"
+    redirect_url = 'available_orders'
+    order_creation_form = OrderCreateForm
+
+    def get(self, request):
+        order_creation_form = self.order_creation_form(initial={'user': request.user})
+        context = {
+            "order": order_creation_form
+        }
+        return render(request, self.template_path, context)
+
+    def post(self, request):
+        order_creation_form = self.order_creation_form(request.POST)
+        if order_creation_form.is_valid():
+            order_instance = order_creation_form.save(commit=False)
+            order_instance.user = request.user
+            order_instance.save()
+            return redirect(self.redirect_url)
+
+        context = {
+            "order": order_creation_form
         }
         return render(request, self.template_path, context)
