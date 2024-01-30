01243445e9e414d4b4cdfcfe0700c961f3f5ecdd
Andrew Tymoshenko <andry925@ukr.net>
2024-01-30 14:31:43 +0200

    Adding non-field validation error

--- library/order/forms.py
+++ library/order/forms.py
@@ -10,5 +10,15 @@ class OrderCreateForm(forms.ModelForm):
 
     def __init__(self, *args, user=None, **kwargs):
         super(OrderCreateForm, self).__init__(*args, **kwargs)
-        if user:
+        self.user = user
+        if self.user:
             self.fields['user'].queryset = User.objects.filter(username=user)
+
+    def clean(self):
+        cleaned_data = super(OrderCreateForm, self).clean()
+        book = cleaned_data.get('book')
+        print(self.user)
+        if Order.objects.filter(book=book).exists() and Order.objects.filter(user=self.user).exists():
+            raise forms.ValidationError(
+                "You can not book this book anymore"
+            )
