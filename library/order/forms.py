36830bced6258df5cf8af46b1245d9db9a1fefed
andrew <andry925@ukr.net>
2024-02-14 21:24:57 +0200

    Added functionality to validate book amount

--- library/order/forms.py
+++ library/order/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from accounts.models import User
+from book.models import Book
 from .models import Order
 
 
@@ -17,5 +18,12 @@ class OrderCreateForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super(OrderCreateForm, self).clean()
         book = cleaned_data.get('book')
+        days = cleaned_data.get('max_days_to_take_book')
+        self.validate_book_amount(book_name=book, days=days)
         if Order.objects.filter(user=self.user, book=book).exists():
             raise forms.ValidationError("You can not book this book any more")
+
+    def validate_book_amount(self, book_name, days):
+        book = Book.objects.get(name=book_name)
+        if book.count <= days:
+            raise forms.ValidationError("We do not have such amount of books")
