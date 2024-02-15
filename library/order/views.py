6bc33dcdf8d6d68e0136f490f642d4ab64adad5b
andrew <andry925@ukr.net>
2024-02-15 22:21:20 +0200

    Adding function to minus book after taking it

--- library/order/views.py
+++ library/order/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from book.models import Book
 from .models import Order
 from .forms import OrderCreateForm
 
@@ -35,6 +36,7 @@ class CreateOrderView(View):
         order_creation_form.user = request.user
         if order_creation_form.is_valid():
             order_instance = order_creation_form.save(commit=False)
+            self.minus_book(order_create_form=order_creation_form)
             order_instance.user = request.user
             order_instance.save()
             return redirect(self.redirect_url)
@@ -42,3 +44,10 @@ class CreateOrderView(View):
             "order": order_creation_form
         }
         return render(request, self.template_path, context)
+
+    def minus_book(self, order_create_form):
+        book_name = order_create_form.cleaned_data.get('book')
+        amount_book = order_create_form.cleaned_data.get('max_days_to_take_book')
+        book_instance = Book.objects.get(name=book_name)
+        book_instance.count -= amount_book
+        book_instance.save()
